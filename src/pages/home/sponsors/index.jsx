87d94336b9a376bbd4effd0b9ec9c1b5e44ef9c2
Varendra Maurya <varendramaurya@gmail.com>
2022-02-04 19:24:03 +0530

    added sponsors

--- src/pages/home/sponsors/index.jsx
+++ src/pages/home/sponsors/index.jsx
@@ -5,6 +5,72 @@ import merchandisePartner from './merchandisePartner.json';
 import inkind from './inkind.json';
 import industryPartner from './industryPartner.json';
 import workshopPartner from './workshopPartner.json';
+import silverSponsor from './silver.json'
+const SponsorSec = ({title,sponsorData}) => {
+	return (
+		<>
+			<h1
+				className="col-12 text"
+				style={{
+					color: '#cd7f32',
+					marginBottom: '30px',
+					textAlign: 'center',
+				}}
+			>
+				{title}
+			</h1>
+			{sponsorData.map((item, index) => (
+				<div
+					className="col-12 col-md-6 col-lg-4"
+					style={{ margin: '0 10px 0 5px' }}
+				>
+					<div
+						className="single-ticket-pricing-table style-2 active text-center mb-100 wow fadeInUp"
+						data-wow-delay="300ms"
+						style={{ padding: '0' }}
+					>
+						<figure
+							className="image"
+							style={{
+								height: '143px',
+								border: '#ffffff',
+							}}
+						>
+							<a
+								href={`${item.link}`}
+								style={{
+									pointerEvents: item.link === '' ? 'none' : '',
+								}}
+							>
+								<div
+									style={{
+										display: 'flex',
+										alignItems: 'center',
+										justifyContent: 'center',
+										width: '100%',
+										height: '100%',
+									}}
+								>
+									<img
+										src={`https://drive.google.com/uc?id=${item.image}`}
+										alt=""
+										style={{
+											height: '123px',
+											width: 'auto',
+											alignItems: 'center',
+											padding: '5px',
+										}}
+									/>
+								</div>
+							</a>
+						</figure>
+					</div>
+				</div>
+			))}
+		</>
+	);
+}
+
 const Sponsors = () => {
 	return (
 		<section className="sponsor">
@@ -25,360 +91,20 @@ const Sponsors = () => {
 							style={{ display: 'flex-wrap', justifyContent: 'center' }}
 						>
 							{/* HIGHLIGHT PLATINUM */}
-							<>
-								<h1
-									className="col-12 text"
-									style={{
-										color: '#cd7f32',
-										marginBottom: '30px',
-										textAlign: 'center',
-									}}
-								>
-									PLATINUM
-								</h1>
-								{platinumSponsors.map((item, index) => (
-									<div
-										className="col-12 col-md-6 col-lg-4"
-										style={{ margin: '0 10px 0 5px' }}
-									>
-										<div
-											className="single-ticket-pricing-table style-2 active text-center mb-100 wow fadeInUp"
-											data-wow-delay="300ms"
-											style={{ padding: '0' }}
-										>
-											<figure
-												className="image"
-												style={{
-													height: '143px',
-													border: '#ffffff',
-												}}
-											>
-												<a
-													href={`${item.link}`}
-													style={{
-														pointerEvents: item.link === '' ? 'none' : '',
-													}}
-												>
-													<div
-														style={{
-															display: 'flex',
-															alignItems: 'center',
-															justifyContent: 'center',
-															width: '100%',
-															height: '100%',
-														}}
-													>
-														<img
-															src={`https://drive.google.com/uc?id=${item.image}`}
-															alt=""
-															style={{
-																height: '123px',
-																width: 'auto',
-																alignItems: 'center',
-																padding: '5px',
-															}}
-														/>
-													</div>
-												</a>
-											</figure>
-										</div>
-									</div>
-								))}
-							</>
+							<SponsorSec title="PLATINUM" sponsorData={platinumSponsors} />
 							{/* HIGHLIGHT GOLD */}
-							<>
-								<h1
-									className="col-12 text"
-									style={{
-										color: '#cd7f32',
-										marginBottom: '30px',
-										textAlign: 'center',
-									}}
-								>
-									GOLD
-								</h1>
-								{goldSponsors.map((item, index) => (
-									<div
-										className="col-12 col-md-6 col-lg-4"
-										style={{ margin: '0 10px 0 5px' }}
-									>
-										<div
-											className="single-ticket-pricing-table style-2 active text-center mb-100 wow fadeInUp"
-											data-wow-delay="300ms"
-											style={{ padding: '0' }}
-										>
-											<figure
-												className="image"
-												style={{
-													height: '143px',
-													border: '#ffffff',
-												}}
-											>
-												<a
-													href={`${item.link}`}
-													style={{
-														pointerEvents: item.link === '' ? 'none' : '',
-													}}
-												>
-													<div
-														style={{
-															display: 'flex',
-															alignItems: 'center',
-															justifyContent: 'center',
-															width: '100%',
-															height: '100%',
-														}}
-													>
-														<img
-															src={`https://drive.google.com/uc?id=${item.image}`}
-															alt=""
-															style={{
-																height: '123px',
-																width: 'auto',
-																alignItems: 'center',
-																padding: '5px',
-															}}
-														/>
-													</div>
-												</a>
-											</figure>
-										</div>
-									</div>
-								))}
-							</>
+							<SponsorSec title="GOLD" sponsorData={goldSponsors} />
+							{/* HIGHLIGHT SILVER */}
+							<SponsorSec title="SILVER" sponsorData={silverSponsor} />
+							{/* HIGHLIGHT INDUSTRIAL LIVE TRAINING AND INTERNSHIP PARTNER */}
+							<SponsorSec title={"INDUSTRIAL LIVE TRAINING AND INTERNSHIP PARTNER"} sponsorData={industryPartner}/>
 							{/* HIGHLIGHT MERCHANDISE */}
-							<>
-								<h1
-									className="col-12 text"
-									style={{
-										color: '#cd7f32',
-										marginBottom: '30px',
-										textAlign: 'center',
-									}}
-								>
-									INDUSTRIAL LIVE TRAINING AND INTERNSHIP PARTNER
-								</h1>
-								{industryPartner.map((item, index) => (
-									<div className="col-12 col-md-6 col-lg-4">
-										<div
-											className="single-ticket-pricing-table style-2 active text-center mb-100 wow fadeInUp"
-											data-wow-delay="300ms"
-											style={{ padding: '0' }}
-										>
-											<figure
-												className="image"
-												style={{
-													height: '143px',
-													border: '#ffffff',
-												}}
-											>
-												<a
-													href={`${item.link}`}
-													style={{
-														pointerEvents: item.link === '' ? 'none' : '',
-													}}
-												>
-													<div
-														style={{
-															display: 'flex',
-															alignItems: 'center',
-															justifyContent: 'center',
-															width: '100%',
-															height: '100%',
-														}}
-													>
-														<img
-															src={`https://drive.google.com/uc?id=${item.image}`}
-															alt=""
-															style={{
-																height: '123px',
-																width: 'auto',
-																alignItems: 'center',
-																padding: '5px',
-															}}
-														/>
-													</div>
-												</a>
-											</figure>
-										</div>
-									</div>
-								))}
-							</>
-							{/* HIGHLIGHT MERCHANDISE */}
-							<>
-								<h1
-									className="col-12 text"
-									style={{
-										color: '#cd7f32',
-										marginBottom: '30px',
-										textAlign: 'center',
-									}}
-								>
-									MERCHANDISE PARTNER
-								</h1>
-								{merchandisePartner.map((item, index) => (
-									<div className="col-12 col-md-6 col-lg-4">
-										<div
-											className="single-ticket-pricing-table style-2 active text-center mb-100 wow fadeInUp"
-											data-wow-delay="300ms"
-											style={{ padding: '0' }}
-										>
-											<figure
-												className="image"
-												style={{
-													height: '143px',
-													border: '#ffffff',
-												}}
-											>
-												<a
-													href={`${item.link}`}
-													style={{
-														pointerEvents: item.link === '' ? 'none' : '',
-													}}
-												>
-													<div
-														style={{
-															display: 'flex',
-															alignItems: 'center',
-															justifyContent: 'center',
-															width: '100%',
-															height: '100%',
-														}}
-													>
-														<img
-															src={`https://drive.google.com/uc?id=${item.image}`}
-															alt=""
-															style={{
-																height: '123px',
-																width: 'auto',
-																alignItems: 'center',
-																padding: '5px',
-															}}
-														/>
-													</div>
-												</a>
-											</figure>
-										</div>
-									</div>
-								))}
-							</>
+							<SponsorSec title="MERCHANDISE PARTNER" sponsorData={merchandisePartner}/>
 
 							{/* HIGHLIGHT WORKSHOP PARTNER */}
-							<>
-								<h1
-									className="col-12 text"
-									style={{
-										color: '#cd7f32',
-										marginBottom: '30px',
-										textAlign: 'center',
-									}}
-								>
-									WORKSHOP PARTNER
-								</h1>
-								{workshopPartner.map((item, index) => (
-									<div className="col-12 col-md-6 col-lg-4">
-										<div
-											className="single-ticket-pricing-table style-2 active text-center mb-100 wow fadeInUp"
-											data-wow-delay="300ms"
-											style={{ padding: '0' }}
-										>
-											<figure
-												className="image"
-												style={{
-													height: '143px',
-													border: '#ffffff',
-												}}
-											>
-												<a
-													href={`${item.link}`}
-													style={{
-														pointerEvents: item.link === '' ? 'none' : '',
-													}}
-												>
-													<div
-														style={{
-															display: 'flex',
-															alignItems: 'center',
-															justifyContent: 'center',
-															width: '100%',
-															height: '100%',
-														}}
-													>
-														<img
-															src={`https://drive.google.com/uc?id=${item.image}`}
-															alt=""
-															style={{
-																height: '123px',
-																width: 'auto',
-																alignItems: 'center',
-																padding: '5px',
-															}}
-														/>
-													</div>
-												</a>
-											</figure>
-										</div>
-									</div>
-								))}
-							</>
+							<SponsorSec title={"WORKSHOP PARTNER"} sponsorData={workshopPartner}/>
 							{/* HIGHLIGHT IN-KIND */}
-							<>
-								<h1
-									className="col-12 text"
-									style={{
-										color: '#cd7f32',
-										marginBottom: '30px',
-										textAlign: 'center',
-									}}
-								>
-									IN-KIND
-								</h1>
-								{inkind.map((item, index) => (
-									<div className="col-12 col-md-6 col-lg-4">
-										<div
-											className="single-ticket-pricing-table style-2 active text-center mb-100 wow fadeInUp"
-											data-wow-delay="300ms"
-											style={{ padding: '0' }}
-										>
-											<figure
-												className="image"
-												style={{
-													height: '143px',
-													border: '#ffffff',
-												}}
-											>
-												<a
-													href={`${item.link}`}
-													style={{
-														pointerEvents: item.link === '' ? 'none' : '',
-													}}
-												>
-													<div
-														style={{
-															display: 'flex',
-															alignItems: 'center',
-															justifyContent: 'center',
-															width: '100%',
-															height: '100%',
-														}}
-													>
-														<img
-															src={`https://drive.google.com/uc?id=${item.image}`}
-															alt=""
-															style={{
-																height: '123px',
-																width: 'auto',
-																alignItems: 'center',
-																padding: '5px',
-															}}
-														/>
-													</div>
-												</a>
-											</figure>
-										</div>
-									</div>
-								))}
-							</>
+							<SponsorSec title={"IN-KIND SPONSOR"} sponsorData={inkind}/>
 						</div>
 					</div>
 				</section>
