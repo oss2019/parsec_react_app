de811942529d166ad9adb123e687a21e5292601d
Varendra Maurya <varendramaurya@gmail.com>
2022-02-05 10:39:35 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import LazyLoad from 'react-lazyload';
 import Routes from './routes';
-
+import ReactGA from 'react-ga';
+const TRACKING_ID = 'UA-219555754-1'; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
 
