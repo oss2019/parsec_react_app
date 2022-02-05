3ecf154f29374f8a94e1b1c3033510064cbba2ae
Varendra Maurya <varendramaurya@gmail.com>
2022-02-05 12:44:44 +0530

    added google universal

--- src/components/Tracker/index.js
+++ src/components/Tracker/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import ReactGA from 'react-ga';
+ReactGA.initialize('UA-219555754-1');
 const RouteChangeTracker = ({ history }) => {
 	history.listen((location, action) => {
 		ReactGA.set({ page: location.pathname });
