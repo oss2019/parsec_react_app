d15bbd7c05da9b3c0f9edef53da4358a4393979e
Varendra Maurya <varendramaurya@gmail.com>
2022-02-17 21:11:09 +0530

    fixed responsive of schedule

--- src/pages/schedule/index.jsx
+++ src/pages/schedule/index.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Header from '../../components/Header';
 import { useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 export default function Schedule() {
 	const [isVisible28, setIsVisible28] = useState(false);
@@ -11,6 +12,20 @@ export default function Schedule() {
 	const [isVisible05, setIsVisible05] = useState(false);
 	const [isVisible06, setIsVisible06] = useState(false);
 	const [isVisible07, setIsVisible07] = useState(false);
+	const isSmallDevice = useMediaQuery({ query: '(max-width:768px' })
+	const containerStyle = {
+		width: isSmallDevice ? '95%' : '60%',
+		border: '2px solid white',
+		borderRadius: '12px',
+		margin: '5px auto',
+		position: 'relative',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		cursor: 'pointer',
+		flexDirection: 'column',
+	};
+	const h5Style = { color: 'inherit',fontSize:isSmallDevice? '15px':''  }
 	return (
 		<div className="page-wrapper">
 			<Header active={21} />
@@ -48,18 +63,7 @@ export default function Schedule() {
 					<br />
 					{/* 28 FEB */}
 					<div
-						style={{
-							width: '60%',
-							border: '2px solid white',
-							borderRadius: '12px',
-							margin: '5px auto',
-							position: 'relative',
-							display: 'flex',
-							alignItems: 'center',
-							justifyContent: 'center',
-							cursor: 'pointer',
-							flexDirection: 'column',
-						}}
+						style={containerStyle}
 						onClick={() => {
 							setIsVisible28(!isVisible28);
 						}}
@@ -70,7 +74,7 @@ export default function Schedule() {
 								style={{
 									color: 'white',
 									transitionDelay: '1s',
-									textAlign: 'left',
+									textAlign: isSmallDevice? 'center': 'left',
 								}}
 							>
 								<br />
@@ -79,7 +83,7 @@ export default function Schedule() {
 									Events
 								</h3>
 
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									ASCENSUS QUIZ ROUND: 08:00 PM TO 11:30 PM
 								</h5>
 							</div>
@@ -87,18 +91,7 @@ export default function Schedule() {
 					</div>
 					{/* 1 MARCH */}
 					<div
-						style={{
-							width: '60%',
-							border: '2px solid white',
-							borderRadius: '12px',
-							margin: '5px auto',
-							position: 'relative',
-							display: 'flex',
-							alignItems: 'center',
-							justifyContent: 'center',
-							cursor: 'pointer',
-							flexDirection: 'column',
-						}}
+						style={containerStyle}
 						onClick={() => {
 							setIsVisible1(!isVisible1);
 						}}
@@ -120,7 +113,7 @@ export default function Schedule() {
 									Events
 								</h3>
 
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									ASCENSUS CASE ROUND: 12:00 AM TO 12:59 PM
 								</h5>
 							</div>
@@ -128,18 +121,7 @@ export default function Schedule() {
 					</div>
 					{/* 2 MARHC */}
 					<div
-						style={{
-							width: '60%',
-							border: '2px solid white',
-							borderRadius: '12px',
-							margin: '5px auto',
-							position: 'relative',
-							display: 'flex',
-							alignItems: 'center',
-							justifyContent: 'center',
-							cursor: 'pointer',
-							flexDirection: 'column',
-						}}
+						style={containerStyle}
 						onClick={() => {
 							setIsVisible2(!isVisible2);
 						}}
@@ -161,7 +143,7 @@ export default function Schedule() {
 									Events
 								</h3>
 
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									DESIGNO PRELIMS BEGINS: 12:00 AM
 								</h5>
 							</div>
@@ -169,18 +151,7 @@ export default function Schedule() {
 					</div>
 					{/* 3 MARHC */}
 					<div
-						style={{
-							width: '60%',
-							border: '2px solid white',
-							borderRadius: '12px',
-							margin: '5px auto',
-							position: 'relative',
-							display: 'flex',
-							alignItems: 'center',
-							justifyContent: 'center',
-							cursor: 'pointer',
-							flexDirection: 'column',
-						}}
+						style={containerStyle}
 						onClick={() => {
 							setIsVisible3(!isVisible3);
 						}}
@@ -202,7 +173,7 @@ export default function Schedule() {
 									Events
 								</h3>
 
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									GUARDAINS KICKSTARTS: 02:00 PM
 								</h5>
 							</div>
@@ -210,18 +181,7 @@ export default function Schedule() {
 					</div>
 					{/* 4 MARHC */}
 					<div
-						style={{
-							width: '60%',
-							border: '2px solid white',
-							borderRadius: '12px',
-							margin: '5px auto',
-							position: 'relative',
-							display: 'flex',
-							alignItems: 'center',
-							justifyContent: 'center',
-							cursor: 'pointer',
-							flexDirection: 'column',
-						}}
+						style={containerStyle}
 						onClick={() => {
 							setIsVisible04(!isVisible04);
 						}}
@@ -242,19 +202,19 @@ export default function Schedule() {
 								<h3 style={{ color: 'inherit', textAlign: 'center' }}>
 									Events
 								</h3>
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									DESIGNO PRELIMS ENDS: 12:00 AM
 								</h5>
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									DEVHACK OPENING CEREMONY: 04:00 PM
 								</h5>
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									DEVHACK ORIENTATION SESSION: 06:00 PM TO 7:30 PM
 								</h5>
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									VENIVIDIVICI KICKSOFF: 08:00 PM
 								</h5>
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									DEVHACK IDEATION PHASE: 08:00 PM
 								</h5>
 							</div>
@@ -262,18 +222,7 @@ export default function Schedule() {
 					</div>
 					{/* 05 MARCH */}
 					<div
-						style={{
-							width: '60%',
-							border: '2px solid white',
-							borderRadius: '12px',
-							margin: '5px auto',
-							position: 'relative',
-							display: 'flex',
-							alignItems: 'center',
-							justifyContent: 'center',
-							cursor: 'pointer',
-							flexDirection: 'column',
-						}}
+						style={containerStyle}
 						onClick={() => {
 							setIsVisible05(!isVisible05);
 						}}
@@ -295,29 +244,29 @@ export default function Schedule() {
 									Events
 								</h3>
 
-								<h5 style={{ color: 'inherit' }}>GUARDAINS ENDS: 12:00 AM</h5>
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>GUARDAINS ENDS: 12:00 AM</h5>
+								<h5 style={h5Style}>
 									ASCENSUS PRESENTATION ROUND KICKSOFF: 06:00 AM
 								</h5>
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									VENIVIDIVICI ENDS: 08:00 AM
 								</h5>
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									DEVHACK SPEAKER SESSION: 11:00 AM
 								</h5>
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									DEVHACK IDEATION JUDGING BEGINS: 11:30 AM
 								</h5>
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									QUIZ PRELIMS: 12:00 PM TO 02:00 PM
 								</h5>
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									DEVHACK INNOVATION PHASE KICKSOFF: 02:00 PM
 								</h5>
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									DESIGNO FINALS: 02:00 PM TO 08:00 PM
 								</h5>
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									DEVHACK POWER HACKING PHASE KICKSOFF: 10:00 PM
 								</h5>
 							</div>
@@ -325,18 +274,7 @@ export default function Schedule() {
 					</div>
 					{/* 06 MARHC */}
 					<div
-						style={{
-							width: '60%',
-							border: '2px solid white',
-							borderRadius: '12px',
-							margin: '5px auto',
-							position: 'relative',
-							display: 'flex',
-							alignItems: 'center',
-							justifyContent: 'center',
-							cursor: 'pointer',
-							flexDirection: 'column',
-						}}
+						style={containerStyle}
 						onClick={() => {
 							setIsVisible06(!isVisible06);
 						}}
@@ -358,31 +296,31 @@ export default function Schedule() {
 									Events
 								</h3>
 
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									DEVHACK SUBMISSION OF PROTOTYPE: 08:00 PM
 								</h5>
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									QUIZ FINALS: 10:00 AM TO 12:30 PM
 								</h5>
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									DEVHACK DECLARATION OF TOP 7 TEAMS: 10:30 AM
 								</h5>
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									ALGOSTRIKE FINAL SCOREBOARD: 11:30 AM
 								</h5>
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									DEVHACK SUBMISSION OF PRESENTATION: 12:15 PM
 								</h5>
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									DEVHACK PRESENTATION OF TOP 7 TEAMS: 12:30 PM
 								</h5>
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									DEVHACK SPEAKER SESSION: 02:30 PM
 								</h5>
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									DEVHACK PRIZE DISTRIBUTION: 03:30 PM
 								</h5>
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									DEVHACK CLOSING CEREMONY: 05:30 PM
 								</h5>
 							</div>
@@ -390,18 +328,7 @@ export default function Schedule() {
 					</div>
 					{/* 07  */}
 					<div
-						style={{
-							width: '60%',
-							border: '2px solid white',
-							borderRadius: '12px',
-							margin: '5px auto',
-							position: 'relative',
-							display: 'flex',
-							alignItems: 'center',
-							justifyContent: 'center',
-							cursor: 'pointer',
-							flexDirection: 'column',
-						}}
+						style={containerStyle}
 						onClick={() => {
 							setIsVisible07(!isVisible07);
 						}}
@@ -423,7 +350,7 @@ export default function Schedule() {
 									Events
 								</h3>
 
-								<h5 style={{ color: 'inherit' }}>
+								<h5 style={h5Style}>
 									ASCENSUS PRESENTATION ROUND ENDS: 12:00 AM
 								</h5>
 							</div>
